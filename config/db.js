4a169815cb0ffb3093c31f0c5520cbc40a04947b
abdallah <30749992+abdalla93@users.noreply.github.com>
2020-03-29 13:12:42 +0200

    expense-v1.333

--- config/db.js
+++ config/db.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const connectDB = () => {
-  mongoose.connect(
-    process.env.MONGO_URI ||
-      "mongodb+srv://admin-abdallah:test123@cluster0-57pd9.mongodb.net/expensetracker?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  );
+  mongoose
+    .connect(
+      process.env.MONGO_URI ||
+        "mongodb+srv://admin-abdallah:test123@cluster0-57pd9.mongodb.net/expensetracker?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    )
+    .catch(error => console.log(error));
 };
 
 module.exports = connectDB;
