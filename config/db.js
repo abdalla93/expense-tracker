2247ee87800629c491b2c029c8d7dad3d5a423b4
abdallah <30749992+abdalla93@users.noreply.github.com>
2020-03-29 12:48:46 +0200

    expense-v1.31

--- config/db.js
+++ config/db.js
@@ -1,22 +1,13 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_URI ||
-        "mongodb+srv://admin-abdallah:test123@cluster0-57pd9.mongodb.net/expensetracker?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
-
-    console.log(`Connected to MongoDB: ${conn.connection.host}`);
-  } catch (err) {
-    console.log(`Error:${err.massage}`);
-    process.exit(1);
-  }
+const connectDB = () => {
+  mongoose.connect(
+    "mongodb+srv://admin-abdallah:test123@cluster0-57pd9.mongodb.net/expensetracker?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
 };
 
 module.exports = connectDB;
